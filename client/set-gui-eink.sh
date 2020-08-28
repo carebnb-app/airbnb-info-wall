0967dc20750f3f7711172c222eb1202ebb7e7bf0
deguilardi <demetrio.guilardi@gmail.com>
2020-08-28 23:41:49 +0100

    commands to change screen

---
+++ client/set-gui-eink.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo raspi-config nonint do_spi 0
+echo "system will reboot now"
+sudo reboot
+exit
+exit
---
+++ client/set-gui-hdmi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo raspi-config nonint do_spi 0
+echo "system will reboot now"
+sudo reboot
+exit
+exit
+
